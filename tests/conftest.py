5831bd82bee246f727fd706086da56113d5a06c0
walle <walle@123.ru>
2022-07-09 15:29:27 +0300

    added selene_1

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+from selene.support.shared import browser
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.browser_name = 'chrome'
+    browser.config.hold_browser_open = True
+    browser.config.window = 1920
+    browser.config.window_width = 1080
