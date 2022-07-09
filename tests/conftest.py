3a78dfada61129651b2e5da775d2aade1654a573
walle <walle@123.ru>
2022-07-09 15:34:38 +0300

    added homework

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ import pytest
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.browser_name = 'chrome'
     browser.config.hold_browser_open = True
-    browser.config.window = 1920
+    browser.config.window_height = 1920
     browser.config.window_width = 1080
